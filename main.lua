2e2cd91ea75c56f740fb5f68f68d8de66621d607
Paulyn <jakeojeff@gmail.com>
2026-03-01 01:25:33 +0530

    Working Stable sky and attached water boxes

--- main.lua
+++ main.lua
@@ -1,29 +1,34 @@
+local push = require "libraries.push"
+
 function love.load()
     layers = {}
     mx, my = 0
     ox, oy = 0
-    wW, wH = love.graphics.getDimensions()
-    love.window.setMode(140, 88)
+    wW, wH = 140, 88
+    push:setupScreen(wW, wH, wW * 4, wH * 4, {fullscreen= false})
+    love.graphics.setDefaultFilter("nearest", "nearest")
+
     stableSky = love.graphics.newImage("assets/watertop/stable.png")
     for i = 1, 4 do
-        table.insert(layers, love.graphics.newImage("assets/watertop/"..i..".png"))
+        table.insert(layers, love.graphics.newImage("assets/watertop/" .. i .. ".png"))
     end
 end
 
-
 function love.update(dt)
-    mx, my = love.mouse.getPosition()
+    mx, my = push:toGame(love.mouse.getPosition())
     ox, oy = distFromCenter(mx, my)
 end
 
 function distFromCenter(x, y)
-    return (wW/2) - x, (wH/2) - y
+    return (wW / 2) - x, (wH / 2) - y
 end
 
 function love.draw()
-    love.graphics.draw(stableSky)
-    for i = 1, #layers do
-        love.graphics.draw(layers[i], ox, oy)
-        
-    end
+    push:start()
+        love.graphics.draw(stableSky, 0, -44 + oy/10)
+        for i = 1, #layers do
+            love.graphics.draw(layers[i], 0, oy/10 * i)
+
+        end
+    push:finish()
 end

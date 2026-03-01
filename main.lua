0420f721d16cbd0a2ac64cc69ae1bfad0c09664d
Paulyn <jakeojeff@gmail.com>
2026-03-01 05:32:29 +0530

    Fixing bob

--- main.lua
+++ main.lua
@@ -9,32 +9,73 @@ function love.load()
         x = 0,
         y = 0
     }
+    baseY = player.y
     ox, oy = 0
     wW, wH = 140, 88
-    push:setupScreen(wW, wH, wW * 4, wH * 4, {fullscreen= false})
+    push:setupScreen(wW, wH, wW * 6, wH * 6, {fullscreen= false})
 
     stableSky = love.graphics.newImage("assets/watertop/stable.png")
     for i = 1, 4 do
         table.insert(layers, love.graphics.newImage("assets/watertop/" .. i .. ".png"))
     end
     buoy = love.graphics.newImage("assets/watertop/buoy.png")
+
+    timer = 0
+end
+
+local push = require "libraries.push"
+
+function love.load()
+        love.graphics.setDefaultFilter("nearest", "nearest")
+
+    layers = {}
+    mx, my = 0
+    player = {
+        x = 0,
+        y = 0
+    }
+    baseY = player.y
+    ox, oy = 0
+    wW, wH = 140, 88
+    push:setupScreen(wW, wH, wW * 6, wH * 6, {fullscreen= false})
+
+    stableSky = love.graphics.newImage("assets/watertop/stable.png")
+    for i = 1, 4 do
+        table.insert(layers, love.graphics.newImage("assets/watertop/" .. i .. ".png"))
+    end
+    buoy = love.graphics.newImage("assets/watertop/buoy.png")
+
+    timer = 0
 end
 
 function love.update(dt)
+    timer = timer + dt
     mx, my = push:toGame(love.mouse.getPosition())
     ox, oy = distFromCenter(player.x, player.y)
 
-    if love.keyboard.isDown("s") then
-        player.y = player.y + 100 * dt
-    end
-        if love.keyboard.isDown("w") then
-        player.y = player.y - 100 * dt
-    end
+    -- Automated bobbing movement
+    local speed = 10     -- How fast it oscillates
+    local amplitude = 12 -- How far it moves up/down
+
+    player.y = baseY + math.sin(timer * speed) * amplitude
 end
 
 function distFromCenter(x, y)
     return (wW / 2) - x, (wH / 2) - y
 end
+
+function love.draw()
+    push:start()
+        love.graphics.draw(stableSky, 0, -44 + oy/10)
+        local horizon = -44 + oy/10 + stableSky:getHeight() - 49
+        for i = 1, #layers do
+            love.graphics.draw(layers[i], 0, (wH - layers[i]:getHeight()) + oy/10 * i)
+
+        end
+        love.graphics.draw(buoy, 0, (wH - layers[#layers]:getHeight()) + oy/10 * #layers - buoy:getHeight())
+    push:finish()
+end
+
 
 function love.draw()
     push:start()
